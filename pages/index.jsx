eb7f4868882f062a614ed122aa984989970567a5
ManupaDev <manupadevsuite@gmail.com>
2022-10-10 19:20:52 +0530

    static home page implemented

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,42 @@
 import Header from "../components/header";
 import ModuleCard from "../components/moduleCard";
+import { VscHome } from "react-icons/vsc";
 
 function Home() {
   return (
-    <div className="min-h-screen border border-red-500 p-4">
-      <Header />
-      <div className="mt-6">
-        <h1 className="text-center text-4xl">Today</h1>
-        <div className=" flex flex-col ">
-          <ModuleCard
-            mode="LEC"
-            venue="4LA"
-            time={{ s: "08:30", e: "10:30" }}
-          />
+    <div className="min-h-screen border border-red-500">
+      <div className="p-4">
+        <Header />
+        <div className="mt-6">
+          <h1 className="text-center text-4xl">Today</h1>
+          <div className="flex flex-col ">
+            <ModuleCard
+              mode="LEC"
+              venue="4LA"
+              time={{ s: "08:30", e: "10:30" }}
+            />
+            <ModuleCard
+              mode="LEC"
+              venue="4LA"
+              time={{ s: "08:30", e: "10:30" }}
+            />
+            <ModuleCard
+              mode="LEC"
+              venue="4LA"
+              time={{ s: "08:30", e: "10:30" }}
+            />
+            <ModuleCard
+              mode="LEC"
+              venue="4LA"
+              time={{ s: "08:30", e: "10:30" }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className=" fixed bottom-0 w-full p-4">
+        <div className="border border-black p-2 rounded-2xl flex justify-center  ">
+          <VscHome size={32} />
         </div>
       </div>
     </div>
