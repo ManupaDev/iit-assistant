77810a9ae2c4b83d1f071d449ff6777142024a5b
ManupaDev <manupadevsuite@gmail.com>
2022-10-17 16:02:01 +0530

    modified pre-alpha ready working only for group "P"

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { getSlotsForToday, getTimetable } from "../utils/api";
 import ModuleCards from "../components/moduleCards";
 import Link from "next/link";
+import Head from "next/head";
 
 function Home() {
   const [slots, setSlots] = useState(null);
@@ -29,57 +30,67 @@ function Home() {
 
   if (user) {
     return (
-      <div className="min-h-screen border border-red-500 flex flex-col justify-between">
-        <div className="p-4">
-          <Header />
-          <div className="mt-6">
-            <h1 className="text-center text-4xl">Today</h1>
-            <ModuleCards slots={slots} />
-          </div>
-          
-          <div
-            className={`w-12 h-12 border border-black fixed bottom-4 right-4 rounded-full z-10 justify-center items-center ${
-              open ? "hidden" : "flex"
-            }`}
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            <HiMenu size={24} />
-          </div>
-          <div
-            className={`w-fit  h-50 border bg-slate-100 border-black fixed bottom-4 right-4  p-2 z-10 rounded-2xl  flex-col justify-between ${
-              open ? "flex" : "hidden"
-            }`}
-          >
-            <div className="flex  flex-col items-end">
-              <Link href="/">
-                <a className="text-xl p-2">Today</a>
-              </Link>
-              <Link href="/timetable">
-                <a className="text-xl p-2">TimeTable</a>
-              </Link>
-              <Link href="/materials">
-                <a className="text-xl p-2">Materials</a>
-              </Link>
-              <Link href="/materials">
-                <a className="text-xl p-2">Profile</a>
-              </Link>
+      <>
+        <Head>
+          <title>IITAssistant | Today</title>
+        </Head>
+        <div className="min-h-screen border border-red-500 flex flex-col justify-between">
+          <div className="p-4">
+            <Header />
+            <div className="mt-6">
+              <h1 className="text-center text-4xl">Today</h1>
+              <ModuleCards slots={slots} />
             </div>
-            <div className="flex justify-end p-2 ">
-              <MdClose
-                size={24}
-                onClick={() => {
-                  setOpen(!open);
-                }}
-              />
+
+            <div
+              className={`w-12 h-12 border border-black fixed bottom-4 right-4 rounded-full z-10 justify-center items-center ${
+                open ? "hidden" : "flex"
+              }`}
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <HiMenu size={24} />
+            </div>
+            <div
+              className={`w-fit  h-50 border bg-slate-100 border-black fixed bottom-4 right-4  p-2 z-10 rounded-2xl  flex-col justify-between ${
+                open ? "flex" : "hidden"
+              }`}
+            >
+              <div className="flex  flex-col items-end">
+                <Link href="/">
+                  <a className="text-xl p-2">Today</a>
+                </Link>
+                <Link href="/timetable">
+                  <a className="text-xl p-2">TimeTable</a>
+                </Link>
+                {/* <Link href="/materials">
+                  <a className="text-xl p-2">Materials</a>
+                </Link>
+                <Link href="/materials">
+                  <a className="text-xl p-2">Profile</a>
+                </Link> */}
+              </div>
+              <div className="flex justify-end p-2 ">
+                <MdClose
+                  size={24}
+                  onClick={() => {
+                    setOpen(!open);
+                  }}
+                />
+              </div>
             </div>
           </div>
-          
+          <h1 className="text-center p-4">
+            Made With 💖 By <span className="font-semibold">ManupaDev</span>
+          </h1>
         </div>
-        <h1 className="text-center p-4">Made With 💖 By <span className="font-semibold">ManupaDev</span></h1>
-      </div>
+      </>
     );
+  } else {
+    <Head>
+      <title>IITAssistant | Today</title>
+    </Head>;
   }
 }
 
--- pages/timetable.jsx
+++ pages/timetable.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { getSlotsForToday, getTimetable } from "../utils/api";
 import Link from "next/link";
 import Switcher from "../components/switcher";
+import Head from "next/head";
+
 
 function Timetable() {
   const [timetable, setTimetable] = useState(null);
@@ -29,58 +31,67 @@ function Timetable() {
 
   if (user && timetable) {
     return (
-      <div className="min-h-screen border border-red-500 flex flex-col justify-between">
-        <div className="p-4">
-          <Header />
-          <div className="mt-6">
-            <h1 className="text-center text-4xl">Timetable</h1>
-            <Switcher timetable={timetable} />
+      <>
+        <Head>
+          <title>IITAssistant | Ttimetable</title>
+        </Head>
+        <div className="min-h-screen border border-red-500 flex flex-col justify-between">
+          <div className="p-4">
+            <Header />
+            <div className="mt-6">
+              <h1 className="text-center text-4xl">Timetable</h1>
+              <Switcher timetable={timetable} />
+            </div>
 
-          </div>
-         
-          <div
-            className={`w-12 h-12 border border-black fixed bottom-4 right-4 rounded-full z-10 justify-center items-center ${
-              open ? "hidden" : "flex"
-            }`}
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            <HiMenu size={24} />
-          </div>
-          <div
-            className={`w-fit  h-50 border bg-slate-100 border-black fixed bottom-4 right-4  p-2 z-10 rounded-2xl  flex-col justify-between ${
-              open ? "flex" : "hidden"
-            }`}
-          >
-            <div className="flex  flex-col items-end">
-              <Link href="/">
-                <a className="text-xl p-2">Today</a>
-              </Link>
-              <Link href="/timetable">
-                <a className="text-xl p-2">TimeTable</a>
-              </Link>
-              <Link href="/materials">
-                <a className="text-xl p-2">Materials</a>
-              </Link>
-              <Link href="/materials">
-                <a className="text-xl p-2">Profile</a>
-              </Link>
+            <div
+              className={`w-12 h-12 border border-black fixed bottom-4 right-4 rounded-full z-10 justify-center items-center ${
+                open ? "hidden" : "flex"
+              }`}
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <HiMenu size={24} />
             </div>
-            <div className="flex justify-end p-2 ">
-              <MdClose
-                size={24}
-                onClick={() => {
-                  setOpen(!open);
-                }}
-              />
+            <div
+              className={`w-fit  h-50 border bg-slate-100 border-black fixed bottom-4 right-4  p-2 z-10 rounded-2xl  flex-col justify-between ${
+                open ? "flex" : "hidden"
+              }`}
+            >
+              <div className="flex  flex-col items-end">
+                <Link href="/">
+                  <a className="text-xl p-2">Today</a>
+                </Link>
+                <Link href="/timetable">
+                  <a className="text-xl p-2">TimeTable</a>
+                </Link>
+                {/* <Link href="/materials">
+                  <a className="text-xl p-2">Materials</a>
+                </Link>
+                <Link href="/materials">
+                  <a className="text-xl p-2">Profile</a>
+                </Link> */}
+              </div>
+              <div className="flex justify-end p-2 ">
+                <MdClose
+                  size={24}
+                  onClick={() => {
+                    setOpen(!open);
+                  }}
+                />
+              </div>
             </div>
           </div>
+          <h1 className="text-center p-4">
+            Made With 💖 By <span className="font-semibold">ManupaDev</span>
+          </h1>
         </div>
-        <h1 className="text-center p-4">Made With 💖 By <span className="font-semibold">ManupaDev</span></h1>
-      
-      </div>
+      </>
     );
+  } else {
+    <Head>
+      <title>IITAssistant | Timetable</title>
+    </Head>;
   }
 }
 
